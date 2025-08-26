c76bc149cdf1ed18c08ae880a787ee038bc3d766
Benslimane Chama <160294271+BenslimaneChama@users.noreply.github.com>
2025-08-27 00:10:13 +0100

    Biocomputing_project.sh

--- Stage0/Biocomputing_project1.sh
+++ Stage0/Biocomputing_project1.sh
@@ -1,4 +1,4 @@
-echo "===== Project 1 ====="
+$echo "===== Project 1 ====="
 #1. Print your name
 $echo "Chama"
 #2. Create a folder titled your name
@@ -34,3 +34,32 @@ $clear && history
 #14. List the files in the two folders and share a screenshot of your terminal
 $cd
 $ls biocomputing/ && ls chama/
+
+
+$echo "===== Project 2 ====="
+#1. Activate your base conda environment
+$conda activate
+#2. Create a conda environment named funtools
+$conda create -n funtools python=3.10
+#3. Activate the funtools environment
+$conda activate funtools
+#4. Install Figlet using conda
+$conda install -c conda-forge figlet
+#6. Install bwa through the bioconda channel
+$conda install -c bioconda bwa
+#7. Install blast through the bioconda channel
+$conda install -c bioconda blast
+#8. Install samtools through the bioconda channel
+$conda install -c bioconda samtools
+#9. Install bedtools through the bioconda channel
+$conda install -c bioconda bedtools
+#10. Install spades.py through the bioconda channel
+$conda install -c bioconda spades
+#11. Install bcftools through the bioconda channel
+$conda install -c bioconda bcftools
+#12. Install fastp through the bioconda channel
+$conda install -c bioconda fastp
+#13. Install multiqc through the bioconda channel
+$conda install -c bioconda multiqc
+
+
